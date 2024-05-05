aab9125eba947997d05ec3db8c38e331e2b875cf
andreciobezerra <andreciobezerra@gmail.com>
2024-05-05 09:27:40 -0300

    add tests flow to subscribe action

--- tests/health_check_tests.rs
+++ tests/health_check_tests.rs
@@ -14,14 +14,58 @@ fn spawn_app() -> String {
 
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let app_address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &app_address))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let payload = "name=xablau%20silva&email=xablauzin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlenconded")
+        .body(payload)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=xablau%20silva", "missing the email"),
+        ("email=xablauzin%20gmail.com", "missing the name"),
+        ("", "Missing both name and email"),
+    ];
+
+    for (invalid_payload, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlenconded")
+            .body(invalid_payload)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
