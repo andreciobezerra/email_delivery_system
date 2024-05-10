78cfc540c95387abf247f8c53276a88e1bbb205c
andreciobezerra <andreciobezerra@gmail.com>
2024-05-10 10:23:10 -0300

    update configs

---
+++ src/configs.rs
@@ -0,0 +1,30 @@
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub host: String,
+    pub database_name: String,
+    pub port: u16,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_configs() -> Result<Settings, config::ConfigError> {
+    config::Config::builder()
+        .add_source(config::File::new("configs.yml", config::FileFormat::Yaml))
+        .build()?
+        .try_deserialize::<Settings>()
+}
