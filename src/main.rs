a94cbccd64a7cce06fdd41e17d59a6ff8993dc0a
andreciobezerra <andreciobezerra@gmail.com>
2024-05-11 11:18:11 -0300

    add database pool of connections

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::net::TcpListener;
 
 use email_delivery_system::configs::get_configs;
 use email_delivery_system::startup::run;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configs = get_configs().expect("Failed to read configs");
     let address = format!("127.0.0.1:{}", configs.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind the port 8000.");
+    let db_connection_pool = PgPool::connect(&configs.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
 
-    run(listener)?.await
+    run(listener, db_connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, HttpResponse};
+use sqlx::{types::chrono::Utc, PgPool};
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +8,22 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let query = sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_pool.get_ref())
+    .await;
+
+    match query {
+        Ok(_) => HttpResponse::Created().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
